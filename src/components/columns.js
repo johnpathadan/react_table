9b72f3a3d8df43b8bb5b83424141a7f642075f43
johnpathadan <johnpathadan@gmail.com>
2022-11-29 13:00:21 +0530

    fix: grouped headers

--- src/components/columns.js
+++ src/components/columns.js
@@ -2,7 +2,7 @@ export const COLUMNS = [
   {
     Header: "Id",
     Footer: "Id",
-    accessor: "id", //this is to map each colum to a specific value in the mock data
+    accessor: "id",
   },
   {
     Header: "First Name",
@@ -29,5 +29,53 @@ export const COLUMNS = [
     Footer: "Phone",
     accessor: "phone",
   },
-  //we can pick what data needs to be rendered in the UI (here we have excluded email and age)
+];
+
+//here we are creating a new array object for grouped headers
+//grouped headers for first name, last name, generic info
+
+export const GROUPED_COLUMNS = [
+  {
+    Header: "Id",
+    Footer: "Id",
+    accessor: "id",
+  },
+  {
+    Header: "Name",
+    Footer: "Name",
+    columns: [
+      //Here we have columns instead of accessor
+      {
+        Header: "First Name",
+        Footer: "First Name",
+        accessor: "first_name",
+      },
+      {
+        Header: "Last Name",
+        Footer: "Last Name",
+        accessor: "last_name",
+      },
+    ],
+  },
+  {
+    Header: "Info",
+    Footer: "Info",
+    columns: [
+      {
+        Header: "Date Of Birth",
+        Footer: "Date Of Birth",
+        accessor: "date_of_birth",
+      },
+      {
+        Header: "Country",
+        Footer: "Country",
+        accessor: "country",
+      },
+      {
+        Header: "Phone",
+        Footer: "Phone",
+        accessor: "phone",
+      },
+    ],
+  },
 ];
